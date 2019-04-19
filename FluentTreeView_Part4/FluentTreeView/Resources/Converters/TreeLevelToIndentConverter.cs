691fe363a1cd52130b4927ed7199a149222514a0
Verrickt <Adios_verrickt@hotmail.com>
2019-04-19 21:11:38 +0800

    Update FluentTreeView Part4

---
+++ FluentTreeView_Part4/FluentTreeView/Resources/Converters/TreeLevelToIndentConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FluentTreeView.Resources.Converters
+{
+    public class TreeLevelToIndentConverter : IValueConverter
+    {
+        public Thickness Margin { get; set; }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TreeViewItem ti)
+            {
+                int level = 0;
+                FrameworkElement current = ti;
+                do
+                {
+                    if(VisualTreeHelper.GetParent(current) is FrameworkElement fe)
+                    {
+                        if (fe is TreeViewItem)
+                        {
+                            level++;
+                        }
+                        if (fe is TreeView)
+                        {
+                            break;
+                        }
+                        current = fe;
+                    }
+                } while (current!=null);
+                return new Thickness(Margin.Left * level, 0, Margin.Right * level, 0);
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
