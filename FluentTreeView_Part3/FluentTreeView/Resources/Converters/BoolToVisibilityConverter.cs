40aa946f362392a6d87b91ce9621c3c86994bcd9
Verrickt <Adios_verrickt@hotmail.com>
2019-04-19 17:22:53 +0800

    Add sample for part3

---
+++ FluentTreeView_Part3/FluentTreeView/Resources/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FluentTreeView.Resources.Converters
+{
+    class BoolToVisibilityConverter : IValueConverter
+    {
+        public bool Inverted { get; set; } = false;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b)
+            {
+                var result = b ^ Inverted;
+                return result ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
